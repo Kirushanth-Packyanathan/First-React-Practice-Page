c5d6cd469be79a72f44bdbc2c83d645456e8a9f6
Kirushanth P <kirushanthp.21@uom.lk>
2024-01-24 09:23:35 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import home from './components/Home';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
